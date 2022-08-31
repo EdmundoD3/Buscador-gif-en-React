5ca40b670d7718c0ddd4bb8e3888ea0309f79801
Edmundo Mendoza <edmundorm1996@gmail.com>
2022-08-31 18:15:16 -0500

    corrigiendo errores y actualizando a el tutorial por errores copie unas partes del tutorial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import ListOfGifs from './components/ListOfGifs';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Home from './pages/Home'
+import SearchResults from './pages/SearchResults'
+import Detail from './pages/Detail'
+import Context from './context/StaticContext'
+import {GifsContextProvider} from './context/GifsContext'
+import { Link, Route } from "wouter"
 
 
-
-function App() {
-  const [gifs, setgifs] = useState([])
-  useEffect(()=>{
-    console.log("actualizando gifs")
-    fetch(apiURL)
-      .then(res => res.json())
-      .then(response => {
-        const {data} = response
-      })
-  },[])
-
+export default function App() {
   return (
-    <div className="App">
-      <section className="App-content">
-        <ListOfGifs keyword="panda"/>
-      </section>
-    </div>
-  );
+  <Context.Provider value={{name: 'midudev',
+  suscribeteAlCanal: true}}>
+      <div className="App">
+        <section className="App-content">
+          <Link to="/">
+            <img className="App-logo" alt='Giffy logo' src='/logo.png' />
+          </Link>
+          <GifsContextProvider>
+            <Route
+              component={Home}
+              path="/"
+            />
+            <Route
+              component={SearchResults}
+              path="/search/:keyword"  />
+            <Route
+              component={Detail}
+              path="/gif/:id"
+            />
+          </GifsContextProvider>
+        </section>
+      </div>
+    </Context.Provider>
+  )
 }
-
-export default App;
---
+++ src/services/getGifs.jsx
@@ -0,0 +1,22 @@
+const apiKey = "RjogUyjU0wvG3m3vlFpvCEclhEGqMhAS&q"
+
+const fromApiResponseToGifs = apiResponse => {
+    const {data = []} = apiResponse
+    if (Array.isArray(data)) {
+      const gifs = data.map(image => {
+        const {images, title, id} = image
+        const { url } = images.downsized_medium
+        return { title, id, url }
+      })
+      return gifs
+    }
+    return []
+  }
+  
+  export default function getGifs ({limit = 25, keyword = 'morty'} = {}) {
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${keyword}&limit=${limit}&offset=0&rating=G&lang=en`
+  
+    return fetch(apiURL)
+      .then(res => res.json())
+      .then(fromApiResponseToGifs)
+  }
